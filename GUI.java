500b8b6968443765b3cfdaf141cbc71427786cdc
sergiomarchena16 <mar16387@uvg.edu.gt>
2018-01-15 12:13:38 -0600

    Add files via upload

---
+++ GUI.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hoja1_radio;
+
+/**
+ *
+ * @author SergioMarchena
+ * @author Mafer Diaz
+ * @version 15/01/2018
+ */
+public class GUI {
+    
+}
