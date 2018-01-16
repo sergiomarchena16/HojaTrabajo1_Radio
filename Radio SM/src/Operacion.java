8e18b16626036e85f445f93f5387fea26f9beaeb
diazMafer <31084398+diazMafer@users.noreply.github.com>
2018-01-16 17:49:31 -0600

    Creacion de clases y GUI

---
+++ Radio SM/src/Operacion.java
@@ -0,0 +1,43 @@
+
+
+/**
+ * @author Maria Fernanda Lopez 17160
+ * @author Sergio Marchena 16
+ * @version 16/01/2018
+ */
+public class Operacion implements Radio{
+    //Atributos 
+    
+
+    @Override
+    public void onOff(boolean e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void Switch() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public float siguiente(float a) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public float anterior(float a) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void guardar(float e, int b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public float seleccionarFav(int b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
---
+++ Radio SM/src/Radio.java
@@ -0,0 +1,22 @@
+
+/**
+ * @author Sergio Marchena 163
+ * @author Maria Fernanda Lopez 17160
+ * @since 15/01/2018 
+ */
+
+public interface Radio {
+    public void onOff(boolean e);
+    
+    public void Switch();
+    
+    public float siguiente(float a);
+    
+    public float anterior(float a);
+    
+    public void guardar(float e, int b);
+    
+    public float seleccionarFav(int b);
+    
+    
+}
