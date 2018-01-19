e7443ee6b4d5a726eb59d6df703f2cf8240d0883
sergiomarchena16 <mar16387@uvg.edu.gt>
2018-01-18 22:44:25 -0600

    3 metodos a testear
    solo usamos 3 métodos para hacer las pruebas, los demás están comentados

--- Radio SM/test/OperacionTest.java
+++ Radio SM/test/OperacionTest.java
@@ -4,10 +4,12 @@
  * and open the template in the editor.
  */
 
+import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 /**
  *
@@ -17,26 +19,37 @@ public class OperacionTest {
     
     public OperacionTest() {
     }
-    
-    
 
-   
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
 
-    
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
 
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
     /**
      * Test of siguiente method, of class Operacion.
      */
     @Test
     public void testSiguiente() {
         System.out.println("siguiente");
-        float a = 0.0F;
+        float a = 540;
         Operacion instance = new Operacion();
-        float expResult = 0.0F;
+        float expResult = 550F;
         float result = instance.siguiente(a);
         assertEquals(expResult, result, 0.0);
         // TODO review the generated test code and remove the default call to fail.
+       if (expResult != result) {
         fail("The test case is a prototype.");
+       }
     }
 
     /**
@@ -45,47 +58,50 @@ public class OperacionTest {
     @Test
     public void testAnterior() {
         System.out.println("anterior");
-        float a = 0.0F;
+        float a = (float) 94.9;
         Operacion instance = new Operacion();
-        float expResult = 0.0F;
+        float expResult = (float) 94.7;
         float result = instance.anterior(a);
         assertEquals(expResult, result, 0.0);
         // TODO review the generated test code and remove the default call to fail.
+        if (expResult != result) {
         fail("The test case is a prototype.");
+       }
     }
 
     /**
      * Test of guardar method, of class Operacion.
      */
-    @Test
-    public void testGuardar() {
-        System.out.println("guardar");
-        float e = 530;
-        int b = 0;
-        Operacion instance = new Operacion();
-        instance.guardar(530, 9);
-        
-    }
+//    @Test
+//    public void testGuardar() {
+//        System.out.println("guardar");
+//        float e = (float) 102.9;
+//        int b =  2;
+//        Operacion instance = new Operacion();
+//        instance.guardar(1029, 2);
+//        
+//    }
 
     /**
      * Test of seleccionarFav method, of class Operacion.
      */
-    @Test
-    public void testSeleccionarFav() {
-        System.out.println("seleccionarFav");
-        int b = 0;
-        Operacion instance = new Operacion();
-        float expResult = 0.0F;
-        float result = instance.seleccionarFav(b);
-        assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+//    @Test
+//    public void testSeleccionarFav() {
+//        System.out.println("seleccionarFav");
+//        int b = 0;
+//        Operacion instance = new Operacion();
+//        float expResult = 0.0F;
+//        float result = instance.seleccionarFav(b);
+//        assertEquals(expResult, result, 0.0);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
 
     /**
      * Test of onOff method, of class Operacion.
      */
-    @Test
+
+/*    @Test
     public void testOnOff() {
         System.out.println("onOff");
         Operacion instance = new Operacion();
@@ -93,7 +109,8 @@ public class OperacionTest {
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
-
+*/
+    
     /**
      * Test of Switch method, of class Operacion.
      */
@@ -101,11 +118,59 @@ public class OperacionTest {
     public void testSwitch() {
         System.out.println("Switch");
         Operacion instance = new Operacion();
-        float expResult = 0.0F;
+        float expResult = (float) 87.9;
         float result = instance.Switch();
         assertEquals(expResult, result, 0.0);
         // TODO review the generated test code and remove the default call to fail.
+        if (expResult != result) {
         fail("The test case is a prototype.");
+        }
+        
+        expResult = (float) 530;
+        result = instance.Switch();
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        if (expResult != result) {
+        fail("The test case is a prototype.");
+        }
     }
+
+    /**
+     * Test of setAM method, of class Operacion.
+     */
+//    @Test
+//    public void testSetAM() {
+//        System.out.println("setAM");
+//        String a = "";
+//        Operacion instance = new Operacion();
+//        instance.setAM(a);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+
+    /**
+     * Test of setFM method, of class Operacion.
+     */
+//    @Test
+//    public void testSetFM() {
+//        System.out.println("setFM");
+//        String a = "";
+//        Operacion instance = new Operacion();
+//        instance.setFM(a);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+
+    /**
+     * Test of onOff method, of class Operacion.
+     */
+//    @Test
+//    public void testOnOff() {
+//        System.out.println("onOff");
+//        Operacion instance = new Operacion();
+//        instance.onOff();
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
     
 }
